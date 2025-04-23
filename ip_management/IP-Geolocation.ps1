b702887981023b7f194608b5d5c04791e7beb6fd
andrewtolbert4 <andrew.tolbert@mymail.champlain.edu>
2025-04-23 16:28:21 -0400

    Update IP-Geolocation.ps1

--- ip_management/IP-Geolocation.ps1
+++ ip_management/IP-Geolocation.ps1
@@ -3,8 +3,14 @@ try {
 
 	$publicIP = Invoke-RestMethod -Uri "https://api64.ipify.org?format=text"
 
+	if (-not $publicIP) {
+		throw "Failed to retrieve public IP Address.
+}
 	$geoInfo = Invoke-RestMethod -Uri "http://ip-api.com/json/$publicIP"
 
+	if (-not $geoInfo) {
+		throw "Failed to retrieve Geolocation information."
+}
 	Write-Host "IP Address: $(geoInfo.query)"
 	Write-Host "Country: $(geoInfo.country)"  
 	Write-Host "Region: $(geoInfo.regionName)"
