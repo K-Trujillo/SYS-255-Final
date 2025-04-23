13109704a8a9bdf8926b5336f174ac712dbed1dc
andrewtolbert4 <andrew.tolbert@mymail.champlain.edu>
2025-04-23 15:17:09 -0400

    Update Change-DHCP-IP.ps1

--- ip_management/Change-DHCP-IP.ps1
+++ ip_management/Change-DHCP-IP.ps1
@@ -1,17 +1,17 @@
 try {
-	Write-Output "Releasing Current Address"
+	Write-Host "Releasing Current Address"
 	ipconfig /release
 
 	Start-Sleep -Seconds 5	
 
-	Write-Output "Requesting New Address"
+	Write-Host "Requesting New Address"
 	ipconfig /renew
 	
 	Start-Sleep -Seconds 3
 	
-	Write-Output ""
-	Write-Output "Here are your new details:"
-	Write-Output ""
+	Write-Host ""
+	Write-Host "Here are your new details:"
+	Write-Host ""
 
 	Start-Sleep -Seconds 2
 
@@ -20,6 +20,6 @@ try {
 	Read-Host "Press Enter to Exit"
 
 	} catch {
-		Write-Output "An error occurred while renewing DHCP:"
-		Write-Output $_.Exception.Message
+		Write-Host "An error occurred while renewing DHCP:"
+		Write-Host $_.Exception.Message
 }
