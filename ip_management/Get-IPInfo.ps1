45ca8ed10379370926b4fe425c04230aff3dea0b
andrewtolbert4 <andrew.tolbert@mymail.champlain.edu>
2025-04-23 14:22:05 -0400

    Update Get-IPInfo.ps1

--- ip_management/Get-IPInfo.ps1
+++ ip_management/Get-IPInfo.ps1
@@ -1,2 +1,17 @@
-$ip = Get-NetIPAddress | Where-Object { $_.AddressFamily -eq "IPv4" }
-$ip | Format-Table
+
+
+try {
+	$ip = Get-NetIPAddress | Where-Object { $._AddressFamily -eq "IPv4" }
+
+	if ($ip) {
+		Write-Output "IP Address Info:"
+		$ip | Format-Table
+
+		} else {
+			throw "No IPv4 Addresses Found."
+	}
+}
+catch {
+	Write-Output "An error occurred while retrieving IP information:"
+	Write-Output $_.Exception.Message
+}
