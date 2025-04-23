a4dad41b162f66d2e3ab55b8b2b4cfbc047bdbac
andrewtolbert4 <andrew.tolbert@mymail.champlain.edu>
2025-04-23 19:02:57 -0400

    Create Live-Network-Traffic.ps1

---
+++ ip_management/Live-Network-Traffic.ps1
@@ -0,0 +1,18 @@
+try { 
+	Write-Host "Monitoring the Network..."
+
+	Get-NetTCPConnection | Select-Object LocalAddress, LocalPort, RemoteAddress, RemotePort, State, Owning Process | Format-Table -AutoSize
+
+	Write-Host "Mapping Connections..."
+	$connections = Get-NetTCPConnection | Select-Object LocalAddress, LocalPort, RemoteAddress, RemotePort, State, OwningProcess
+
+	foreach ($conn in $connections) {
+		$processName = (Get-Process -Id $conn.OwningProcess).ProcessName
+		Write-Host "PID $($conn.OwningProcess) $processName > $($conn.RemoteAddress):$($conn.RemotePort) ($($conn.State))"
+}
+
+	Read-Host "Press Enter to Exit"
+} catch {
+	Write-Host "An error has occurred."
+	Write-Host $_.Exception.Message
+}
