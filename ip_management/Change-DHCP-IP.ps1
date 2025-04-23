93308955e5efc863cf028b9389814b4f3eee7b14
andrewtolbert4 <andrew.tolbert@mymail.champlain.edu>
2025-04-23 15:13:53 -0400

    Update Change-DHCP-IP.ps1

--- ip_management/Change-DHCP-IP.ps1
+++ ip_management/Change-DHCP-IP.ps1
@@ -9,11 +9,15 @@ try {
 	
 	Start-Sleep -Seconds 3
 	
-	Write-Output "'nHere are your new details:'n"
-	
+	Write-Output ""
+	Write-Output "Here are your new details:"
+	Write-Output ""
+
+	Start-Sleep -Seconds 2
+
 	Get-NetIPConfiguration | Select-Object IPv4Address, InterfaceAlias, DNSServer
 	
-	Read-Host "'nPress Enter to Exit'n"
+	Read-Host "Press Enter to Exit"
 
 	} catch {
 		Write-Output "An error occurred while renewing DHCP:"
