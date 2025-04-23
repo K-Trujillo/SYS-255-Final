f5568c0ff365599d6ac9b128fc871932ab45f7f8
andrewtolbert4 <andrew.tolbert@mymail.champlain.edu>
2025-04-23 15:35:43 -0400

    Create Private-Public-IPs.ps1

---
+++ ip_management/Private-Public-IPs.ps1
@@ -0,0 +1,15 @@
+try {
+	Write-Output "Searching for IP Addresses."
+
+	$privateIP = Get-NetIPAddress | Where-Object { $_.AddressFamily -eq "IPv4" -and $_.InterfaceAlias -notlike "Loopback*" } | Select-Object -ExpandProperty IPv4Address
+	Write-Output "Private IP Address:$privateIP"
+
+	$publicIP = Invoke-RestMethod -Uri "https://Champlain.edu"
+	Write-Output "Public IP Address:$publicIP"
+
+	Read-Host "Press Enter to Exit"
+
+} catch {
+	Write-Output "An error has occurred"
+	Write-Output $_.Exception.Message
+}
