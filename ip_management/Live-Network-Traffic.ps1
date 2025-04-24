5ec594d91412ffc56d2789f60a8cf75435867adb
andrewtolbert4 <andrew.tolbert@mymail.champlain.edu>
2025-04-23 21:10:21 -0400

    Changes

--- ip_management/Live-Network-Traffic.ps1
+++ ip_management/Live-Network-Traffic.ps1
@@ -6,10 +6,7 @@ try {
 	Write-Host "Mapping Connections..."
 	$connections = Get-NetTCPConnection | Select-Object LocalAddress, LocalPort, RemoteAddress, RemotePort, State, OwningProcess
 
-	foreach ($conn in $connections) {
-		$processName = (Get-Process -Id $conn.OwningProcess).ProcessName
-		Write-Host "PID $($conn.OwningProcess) $processName > $($conn.RemoteAddress):$($conn.RemotePort) ($($conn.State))"
-}
+	
 
 	Read-Host "Press Enter to Exit"
 } catch {
